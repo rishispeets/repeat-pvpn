cf9c62913a4dea0df616b0df777894212ed037ec
rishispeets <hello@rishispeets.com>
2019-03-10 17:14:09 +0100

    add repeatConnAttempts

--- main.go
+++ main.go
@@ -3,26 +3,36 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"time"
 )
 
 func main() {
-	execPvpn()
+	repeatConnAttempts()
+}
+
+func repeatConnAttempts() {
+	for {
+		connResult, connError := execPvpn()
+		if connError != nil {
+			// Log message and let loop retry conn after short wait
+			printErr(connError)
+			time.Sleep(1 * time.Second)
+		} else {
+			fmt.Printf("%s", connResult)
+			break
+		}
+	}
 }
 
 // execPvpn returns the result of a protonvpn connection attempt
-func execPvpn() {
-	cmd := exec.Command("/bin/sh", "-c", "sudo pvpn -f")
-	out, err := cmd.CombinedOutput()
-
-	if err != nil {
-		printErr(err)
-	}
-
-	fmt.Printf("%s", out)
+func execPvpn() (out []byte, err error) {
+	cmd := exec.Command("/bin/sh", "-c", "pvpn -f")
+	out, err = cmd.CombinedOutput()
+	return
 }
 
-// printErr prints the given error message
 func printErr(err error) {
-	fmt.Println("Error occurred")
+	fmt.Println("Error occurred: ")
 	fmt.Printf("%s", err)
+	fmt.Println("\nRetrying...")
 }
